1584b98d4c6dd594aebf9b39be1b15307b4609b9
GreenBean0303 <lootsmannagnes@gmail.com>
2025-09-09 14:57:56 +0300

    Display all articles using the article db model

---
+++ models/article.js
@@ -0,0 +1,19 @@
+const BaseSQLModel = require('./base');
+
+class ArticleModel extends BaseSQLModel {
+  constructor() {
+    super('article'); 
+  }
+  async findAll() {
+    const articles = await super.findAll();
+    return articles;
+  }
+
+  async findOne(slug) {
+    const articles = await super.findOne('slug', slug);
+    return articles;
+  }
+
+}
+
+module.exports = ArticleModel;
---
+++ utils/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql_oop'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id', conn.threadId);
+});
+
+module.exports = conn;
